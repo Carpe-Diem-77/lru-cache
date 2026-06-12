7f3e29581ce49ba1872ca3ae2f0fd458dc9e4ac6
github-action-benchmark <github@users.noreply.github.com>
2026-06-12 08:32:19 +0000

    add LRU Cache Benchmark (googlecpp) benchmark result for 921bb883220e0c8e943032ddad1aaa7294542b5f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781251787417,
+  "lastUpdate": 1781253139848,
   "repoUrl": "https://github.com/Carpe-Diem-77/lru-cache",
   "entries": {
     "LRU Cache Benchmark": [
@@ -240,6 +240,144 @@ window.BENCHMARK_DATA = {
             "value": 42.51358016849781,
             "unit": "ns/iter",
             "extra": "iterations: 16499648\ncpu: 42.50798053388775 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "33997690+DeepWaterWhale@users.noreply.github.com",
+            "name": "DeepWaterWhale",
+            "username": "DeepWaterWhale"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "921bb883220e0c8e943032ddad1aaa7294542b5f",
+          "message": "Add bench with claude (#16)\n\n* Add bench with claude\n\n* Fix gcovr parse error",
+          "timestamp": "2026-06-12T16:31:38+08:00",
+          "tree_id": "ecf4d0c77d8f3b1d44c120b1e1bea2a9cb8a9c6b",
+          "url": "https://github.com/Carpe-Diem-77/lru-cache/commit/921bb883220e0c8e943032ddad1aaa7294542b5f"
+        },
+        "date": 1781253138964,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_GetHit",
+            "value": 15.095993659126057,
+            "unit": "ns/iter",
+            "extra": "iterations: 46804904\ncpu: 15.094269758570597 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_GetHit_Baseline",
+            "value": 11.218078341186738,
+            "unit": "ns/iter",
+            "extra": "iterations: 60878705\ncpu: 11.216395404600018 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_GetMiss",
+            "value": 7.811505553667488,
+            "unit": "ns/iter",
+            "extra": "iterations: 89995304\ncpu: 7.810831107365333 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_GetMiss_Baseline",
+            "value": 10.974527759817345,
+            "unit": "ns/iter",
+            "extra": "iterations: 63627580\ncpu: 10.97318217980316 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_PutUnderCapacity",
+            "value": 25.771696193691774,
+            "unit": "ns/iter",
+            "extra": "iterations: 27224833\ncpu: 25.77041596545329 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_PutUnderCapacity_Baseline",
+            "value": 7.173952551090387,
+            "unit": "ns/iter",
+            "extra": "iterations: 97307610\ncpu: 7.173592928651723 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_PutEviction",
+            "value": 65.75646589013064,
+            "unit": "ns/iter",
+            "extra": "iterations: 10640337\ncpu: 65.74816323956662 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Emplace",
+            "value": 26.874765834139,
+            "unit": "ns/iter",
+            "extra": "iterations: 26021086\ncpu: 26.870373626988506 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Put",
+            "value": 25.64639027780935,
+            "unit": "ns/iter",
+            "extra": "iterations: 27278789\ncpu: 25.643245746722858 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Mixed8020Zipfian",
+            "value": 80.24334891024336,
+            "unit": "ns/iter",
+            "extra": "iterations: 8791003\ncpu: 80.23232047583203 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Mixed8020Zipfian_Baseline",
+            "value": 64.70356062452326,
+            "unit": "ns/iter",
+            "extra": "iterations: 10795887\ncpu: 64.68973359947162 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_ConcurrentSharedCache/threads:1",
+            "value": 67.0086479402811,
+            "unit": "ns/iter",
+            "extra": "iterations: 10492325\ncpu: 66.98900510611323 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_ConcurrentSharedCache/threads:2",
+            "value": 559.0852319161879,
+            "unit": "ns/iter",
+            "extra": "iterations: 1260326\ncpu: 554.842594693754 ns\nthreads: 2"
+          },
+          {
+            "name": "BM_ConcurrentSharedCache/threads:4",
+            "value": 951.3541331542589,
+            "unit": "ns/iter",
+            "extra": "iterations: 1069256\ncpu: 649.7087442109266 ns\nthreads: 4"
+          },
+          {
+            "name": "BM_ConcurrentSharedCache/threads:8",
+            "value": 2245.3369153243166,
+            "unit": "ns/iter",
+            "extra": "iterations: 650104\ncpu: 1062.2825717116027 ns\nthreads: 8"
+          },
+          {
+            "name": "BM_ConcurrentSharedCache_Baseline/threads:1",
+            "value": 61.36007850811433,
+            "unit": "ns/iter",
+            "extra": "iterations: 11398567\ncpu: 61.35377157497065 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_ConcurrentSharedCache_Baseline/threads:2",
+            "value": 124.50092184858141,
+            "unit": "ns/iter",
+            "extra": "iterations: 5060484\ncpu: 124.19744692404919 ns\nthreads: 2"
+          },
+          {
+            "name": "BM_ConcurrentSharedCache_Baseline/threads:4",
+            "value": 371.79270392332023,
+            "unit": "ns/iter",
+            "extra": "iterations: 2151348\ncpu: 313.17712197189843 ns\nthreads: 4"
+          },
+          {
+            "name": "BM_ConcurrentSharedCache_Baseline/threads:8",
+            "value": 759.5377117207556,
+            "unit": "ns/iter",
+            "extra": "iterations: 1864248\ncpu: 377.0831992310032 ns\nthreads: 8"
           }
         ]
       }
